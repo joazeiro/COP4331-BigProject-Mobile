33e483f45a67a0f79e47e4cde7f29674d5ea13bc
cristiamenciso <encisocristiam@gmail.com>
2023-07-17 22:55:27 -0400

    added a SearchContext and Navbar component

---
+++ cop4331-bigproject-mobile/components/Navbar.js
@@ -0,0 +1,113 @@
+import React, { useState, useEffect, useContext } from 'react';
+import { View, TextInput, TouchableOpacity, Text, AsyncStorage } from 'react-native';
+import { useNavigation } from '@react-navigation/native';
+import { SearchContext } from 'cop4331-bigproject-mobile\components\SearchContext.js'; 
+
+const Navbar = () => {
+    const navigation = useNavigation();
+    const [isLoggedIn, setIsLoggedIn] = useState(false);
+    const [searchQuery, setSearchQuery] = useContext(SearchContext);
+    const apiUrl = "https://geobooks-e802c07bfa62.herokuapp.com"; 
+
+    useEffect(() => 
+    {
+        const checkLoginStatus = async () => 
+        {
+            let token = null;
+
+            try {
+                token = await AsyncStorage.getItem('personalToken');
+            } catch (e) {
+                console.error(e);
+            }
+
+            try
+            {
+                if (!token)
+                {
+                    setIsLoggedIn(false);
+                    return;
+                }
+
+                const response = await fetch(apiUrl + '/',
+                {
+                    method: 'POST',
+                    headers:
+                    {
+                        'Content-Type': 'application/json'
+                    },
+                    body: JSON.stringify(
+                        {
+                            token: token
+                        }
+                    )
+                });
+
+                if (response.ok)
+                {
+                    setIsLoggedIn(true);
+                }
+                else 
+                {
+                    setIsLoggedIn(false);
+                }
+            }
+            
+            catch
+            {
+                setIsLoggedIn(false);
+            }
+        };
+
+        checkLoginStatus();
+
+    }, []);
+
+    const handleSearch = (value) =>
+    {
+        setSearchQuery(value);
+    }
+
+    return (
+        <View style={{ flexDirection: 'row', justifyContent: 'space-between' }}>
+            <Text style={{ fontSize: 24 }}>GeoBook</Text>
+            <View>
+                <TextInput
+                    style={{ height: 40, borderColor: 'gray', borderWidth: 1 }}
+                    placeholder = "Search"
+                    value = {searchQuery}
+                    onChangeText = {handleSearch}
+                />
+            </View>
+            <View style={{ flexDirection: 'row' }}>
+                {!isLoggedIn ? (
+                    <>
+                        <TouchableOpacity onPress={() => navigation.navigate('Login')}>
+                            <Text>Login</Text>
+                        </TouchableOpacity>
+                        <TouchableOpacity onPress={() => navigation.navigate('Register')}>
+                            <Text>Sign Up</Text>
+                        </TouchableOpacity>
+                    </>
+                ) : (
+                    <>
+                        <TouchableOpacity onPress={() => navigation.navigate('CreatePost')}>
+                            <Text>Create Post</Text>
+                        </TouchableOpacity>
+                        <TouchableOpacity onPress={() => navigation.navigate('MyAccount')}>
+                            <Text>My Account</Text>
+                        </TouchableOpacity>
+                        <TouchableOpacity onPress={async () => {
+                            await AsyncStorage.setItem('personalToken', '');
+                            navigation.navigate('Login');
+                        }}>
+                            <Text>Sign Out</Text>
+                        </TouchableOpacity>
+                    </>
+                )}
+            </View>
+        </View>
+    );
+};
+
+export default Navbar;
