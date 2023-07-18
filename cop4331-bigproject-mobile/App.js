e3259254b1d877b7daca7555fef2ff4d460b54b2
cristiamenciso <encisocristiam@gmail.com>
2023-07-18 11:38:00 -0400

    background image in all current screens.

--- cop4331-bigproject-mobile/App.js
+++ cop4331-bigproject-mobile/App.js
@@ -1,23 +1,33 @@
 import React from 'react';
+import { ImageBackground, StyleSheet } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack'; 
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
-import LoginPage from 'cop4331-bigproject-mobile/login/pages.js'; // Ensure this path is correct for your project structure
+import LoginPage from 'cop4331-bigproject-mobile/login/pages.js'; // Ensure this path is correct
 import RegisterPage from 'cop4331-bigproject-mobile/register/pages.js'; // Adjust the path as needed
 import ForgotPasswordPage from './login/forgot-password/pages';
-
-
+import backgroundImage from 'cop4331-bigproject-mobile/assets/thank-god-we-found-it.png';
 
 const Stack = createStackNavigator();
+
+const CustomScreen = ({ children }) => (
+  <ImageBackground source={backgroundImage} style={styles.backgroundImage}>
+    {children}
+  </ImageBackground>
+);
 
 export default function App() {
   return (
     <NavigationContainer>
       <Stack.Navigator initialRouteName="Login">
-        <Stack.Screen name="Login" component={LoginPage} />
-        <Stack.Screen name="Register" component={RegisterPage} />
-        <Stack.Screen name="ForgotPassword" component={ForgotPasswordPage} />
+        <Stack.Screen name="Login">
+          {props => <CustomScreen><LoginPage {...props} /></CustomScreen>}
+        </Stack.Screen>
+        <Stack.Screen name="Register">
+          {props => <CustomScreen><RegisterPage {...props} /></CustomScreen>}
+        </Stack.Screen>
+        <Stack.Screen name="ForgotPassword">
+          {props => <CustomScreen><ForgotPasswordPage {...props} /></CustomScreen>}
+        </Stack.Screen>
         {/* Add other screens as needed */}
         {/* <Stack.Screen name="OtherScreen" component={OtherScreen} /> */} 
       </Stack.Navigator>
@@ -26,10 +36,9 @@ export default function App() {
 }
 
 const styles = StyleSheet.create({
-  container: {
+  backgroundImage: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
+    resizeMode: 'cover', // cover or contain its upto you view look
     justifyContent: 'center',
   },
 });
--- cop4331-bigproject-mobile/login/pages.js
+++ cop4331-bigproject-mobile/login/pages.js
@@ -3,6 +3,7 @@ import FormContainer from "cop4331-bigproject-mobile/components/FormContainer.js
 import LoginForm from "cop4331-bigproject-mobile/components/LoginForm.js";
 import FormTitle from "cop4331-bigproject-mobile/components/FormTitle.js";
 
+
 export default function LoginPage({ navigation }) { // Add navigation prop here
   return (
     <FormContainer>
